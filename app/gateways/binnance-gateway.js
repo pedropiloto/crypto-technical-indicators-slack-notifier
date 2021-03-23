338b08d33809ced2e3a073f455565fe7cbfa2fca
Pedro Piloto <pedro@pedropiloto.com>
2021-03-23 00:54:11 +0000

    migrate from finnhub to binance API

---
+++ app/gateways/binnance-gateway.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const axios = require('axios');
+const Bottleneck = require('bottleneck');
+
+const limiter = new Bottleneck({
+  reservoir: 40, // initial value
+  reservoirRefreshAmount: 40,
+  reservoirRefreshInterval: 60 * 1000, // must be divisible by 250
+
+  // also use maxConcurrent and/or minTime for safety
+  maxConcurrent: 1,
+  minTime: 1000, // pick a value that makes sense for your use case
+});
+
+// eslint-disable-next-line import/prefer-default-export
+const getCandles = async (symbol) => limiter.wrap(() => axios({
+  method: 'get',
+  url: `https://api.binance.com/api/v3/klines?symbol=${symbol}&limit=200&interval=4h`,
+}))();
+
+const getQuote = async (symbol) => limiter.wrap(() => axios({
+  method: 'get',
+  url: `https://api.binance.com/api/v3/ticker/price?symbol=${symbol}`,
+}))();
+
+module.exports = {
+  getQuote, getCandles,
+};
--- app/main.js
+++ app/main.js
@@ -1,20 +1,21 @@
 const Bugsnag = require('@bugsnag/js');
 require('dotenv').config();
-const { log, sendAndCloseLogzio } = require('./utils/logger');
+const { RSI } = require('technicalindicators');
+const { BollingerBands, SMA } = require('trading-signals');
+const { log } = require('./utils/logger');
 const {
   OPERATIONAL_LOG_TYPE: OPERATIONAL,
   ERROR_SEVERITY: ERROR,
 } = require('./utils/constants');
 const { decimalAdjust } = require('./utils/calculation');
-const startClient = require('./client');
-const {
-  getRSI, getBollingerBands, getSMA,
-} = require('./gateways/finnhub-gateway');
 const {
   publishSellAlert, publishBuyAlert,
 } = require('./gateways/slack-gateway');
 const { SELL_RULE, BUY_RULE } = require('./utils/rules');
 const DecisionEngine = require('./decision-engine');
+const { getQuote, getCandles } = require('./gateways/binnance-gateway');
+
+const delay = (interval) => new Promise((resolve) => setTimeout(resolve, interval));
 
 const proccessResults = (indicatorResults) => {
   try {
@@ -52,68 +53,100 @@ const proccessResults = (indicatorResults) => {
   }
 };
 
-const analyseSymbol = async (symbol, lastPrice) => {
-  let rsiResponse; let
-    bollingerBandsResponse; let sma50Response;
+const analyseSymbol = async (symbol) => {
+  let candles; let quote;
+
   try {
-    rsiResponse = await getRSI(symbol);
+    quote = (await getQuote(symbol)).data.price;
   } catch (e) {
     log({
-      message: 'Error during retrieving RSI indicator', error: e.toString(), errorr_trace: e.stack, type: OPERATIONAL, transactional: false, severity: ERROR,
-    });
-    return;
-  }
-  try {
-    bollingerBandsResponse = await getBollingerBands(symbol);
-  } catch (e) {
-    log({
-      message: 'Error during retrieving BollingerBands indicator', error: e.toString(), errorr_trace: e.stack, type: OPERATIONAL, transactional: false, severity: ERROR,
+      message: 'Error during retrieving quote', error: e.toString(), errorr_trace: e.stack, type: OPERATIONAL, transactional: false, severity: ERROR,
     });
     return;
   }
   try {
-    sma50Response = await getSMA(symbol, 50);
+    candles = (await getCandles(symbol)).data.map((x) => x[4]);
   } catch (e) {
     log({
-      message: 'Error during retrieving SMA 50 indicator', error: e.toString(), errorr_trace: e.stack, type: OPERATIONAL, transactional: false, severity: ERROR,
+      message: 'Error during retrieving candles', error: e.toString(), errorr_trace: e.stack, type: OPERATIONAL, transactional: false, severity: ERROR,
     });
-    return;
   }
 
-  const rsiValues = rsiResponse.data.rsi;
+  const rsiArray = RSI.calculate({
+    values: candles,
+    period: 14,
+  });
+  const rsiValue = decimalAdjust('floor', rsiArray[rsiArray.length - 1], -2);
+  console.log('rsi', symbol, rsiValue);
 
-  if (!rsiValues) {
-    log({
-      message: 'RSI has no value for some reason', type: OPERATIONAL, transactional: true, severity: ERROR,
-    });
-    return;
-  }
-  const rsiValue = decimalAdjust('floor', rsiValues[rsiValues.length - 1], -2);
+  const bb = new BollingerBands(14, 2);
 
-  const bollingerBandsLower = decimalAdjust('floor', bollingerBandsResponse.data.lowerband[
-    bollingerBandsResponse.data.upperband.length - 1
-  ], -6);
+  candles.forEach((x) => bb.update(x));
 
-  const bollingerBandsUpper = decimalAdjust('floor', bollingerBandsResponse.data.upperband[
-    bollingerBandsResponse.data.upperband.length - 1
-  ], -6);
+  const bollingerBandsLower = decimalAdjust('floor', bb.getResult().lower.valueOf(), -6);
 
-  const sma50 = decimalAdjust('floor', sma50Response.data.sma[sma50Response.data.sma.length - 1], -6);
+  const bollingerBandsUpper = decimalAdjust('floor', bb.getResult().upper.valueOf(), -6);
 
-  const currentQuote = lastPrice;
+  const sma50 = new SMA(50);
+  candles.slice(candles.length - 50, candles.length).forEach((x) => sma50.update(x));
+
+  const sma50Value = decimalAdjust('floor', sma50.getResult().valueOf(), -6);
+
+  // const sma200 = new SMA(200);
+  // candles.slice(candles.length - 200, candles.length).forEach((x) => sma200.update(x));
+  //
+  // const sma200Value = decimalAdjust('floor', sma200.getResult().valueOf(), -6);
 
   proccessResults({
     symbol,
     rsi: rsiValue,
     bb_upper: bollingerBandsUpper,
     bb_lower: bollingerBandsLower,
-    current_quote: currentQuote,
-    sma50,
+    current_quote: quote,
+    sma50: sma50Value,
   });
+};
+
+const start = async () => {
+  const symbols = process.env.SYMBOLS.split(',');
+  while (true) {
+    log({
+      message: 'starting symbols analysis loop', type: OPERATIONAL, transactional: false,
+    });
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < symbols.length; i++) {
+      log({
+        message: `analysing ${symbols[i]}`, type: OPERATIONAL, transactional: true,
+      });
+      // eslint-disable-next-line no-await-in-loop
+      await analyseSymbol(symbols[i]);
+    }
+    log({
+      message: 'finishing symbols analysis loop', type: OPERATIONAL, transactional: false,
+    });
+    // eslint-disable-next-line no-await-in-loop
+    await delay(72000);
+  }
 };
 
 if (process.env.BUSGNAG_API_KEY) {
   Bugsnag.start({ apiKey: `${process.env.BUSGNAG_API_KEY}` });
 }
 
-startClient(analyseSymbol);
+try {
+  start();
+} catch (exception) {
+  const message = 'Error occured in bot, shutting down. Check the logs for more information.';
+  const error = exception.toString();
+  const stackTrace = exception.stack;
+
+  log({
+    message,
+    error,
+    errorr_trace: stackTrace,
+    type: OPERATIONAL,
+    transactional: true,
+    severity: ERROR,
+  });
+  start();
+}
